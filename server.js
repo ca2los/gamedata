56b3a505fbba3b8d816f86c70ecf22a643f4df46
ca2los <{ID}+{username}@users.noreply.github.com>
2021-09-17 16:02:47 -0500

    Major changes in the queries

--- server.js
+++ server.js
@@ -1,45 +1,33 @@
     // NPM: Packages
-    const express = require('express');
     const mysql = require('mysql2');
     const inquirer = require('inquirer');
-    const fs = require('fs');
-    const path = require('path');
-    const cTable = require('console.table');
-    const term = require("terminal-kit").terminal;
-    const util = require("util");
+    const Table = require('easy-table');
 
     // IMPORT: Local files
     const banner = require('./banner/banner');
-    const {application} = require("express");
-
-    const OUTPUT_DIR = path.resolve(__dirname, "dev");
-    const outputPath = path.join(OUTPUT_DIR, 'index.html');
-    const render = require('./js/html.js');
-    const Manager = require("./js/profile_01");
-
-    // Empty arrays to retrieve data from the program
-    const save_VG = [];
-
-    // LOCAL: Computer connection
-    const PORT = process.env.PORT || 3001;
-    const app = express();
-
-    // PARSING: Data manipulation (retrieves) @ INSOMNIA
-    app.use(express.urlencoded({extended:false}));
-    app.use(express.json());
+    const VideoGame = require("./constructor/profile_01");
 
     // SERVER: Credentials
-    const db = mysql.createConnection(
+    const connection = mysql.createConnection(
         {
             host: 'localhost',
+            port: 3001,
             user: 'root',
             password: "",
             database: 'game_db',
         }
     );
 
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected as id " + connection.threadId + "\n");
+        managerView();
+    })
+
     // QUESTIONS: Add a video game
-    function appMenu() {
+    function managerView() {
+
+        console.log(banner);
 
         function start_program() {
             inquirer.prompt([
@@ -92,203 +80,186 @@
         }
 
         function video_game() {
-            console.log("Add the video game");
             inquirer.prompt([
                 {
-                    type: 'input',
-                    name: 'name_videogame',
-                    message: 'Name of the video game:',
-                    validate: answer => {
-                        if (answer !== "") {
-                            return true;
-                        }
-                        return "Enter a GitHub user.";
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'year_videogame',
-                    message: 'Year of release',
-                    validate: answer => {
-                        const pass = answer.match(
-                            /^[0-9]\d*$/
-                        );
-                        if (pass) {
-                            return true;
-                        }
-                        return "Enter a number value.";
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'console_name',
-                    message: 'Name of the console',
-                    validate: answer => {
-                        if (answer !== "") {
-                            return true;
-                        }
-                        return "Enter the name of the console.";
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'console_brand',
-                    message: 'Name of the brand:',
-                    validate: answer => {
-                        if (answer !== "") {
-                            return true;
-                        }
-                        return "Enter the name of the brand.";
-                    }
-                },
-                {
-                    type: 'confirm',
-                    name: 'game_format',
-                    message: 'Is the game in digital format?',
-                },
-                {
-                    type: 'input',
-                    name: 'release_price',
-                    message: 'Price on release day: $',
-                    validate: answer => {
-                        const pass = answer.match(
-                            /^[0-9]\d*$/
-                        );
-                        if (pass) {
-                            return true;
-                        }
-                        return "Enter a number value.";
-                    }
-                },
-                {
-                    type: 'input',
-                    name: 'rating_videogame',
-                    message: 'ESRB rating:',
-                    validate: answer => {
-                        const pass = answer.match(
-                            /^[0-9]\d*$/
-                        );
-                        if (pass) {
-                            return true;
-                        }
-                        return "Enter a number value.";
-                    }
+                    message: "View options:",
+                    type: "list",
+                    choices: ["Show all video games", "Filter video games by year", "Update video game data", "Add a new video game", "Exit \n"],
+                    name: "task"
+
                 }
-            ]).then(answers => {
-                const videogame = new VideoGame(
-                    answers.name_videogame,
-                    answers.year_videogame,
-                    answers.console_name,
-                    answers.console_brand,
-                    answers.game_format,
-                    answers.release_price,
-                    answers.rating_videogame,
-                );
-                save_VG.push(manager);
-                now_create();
-            });
+            ]).then(function (answer) {
+                var task = answer.task;
+                switch (task) {
+                    case "Show all video games":
+                        orderBy_all();
+                        break;
+                    case "Filter video games by year":
+                        orderBy_year();
+                        break;
+                    case "Update video game data":
+                        updateGame();
+                        break;
+                    case "Add a new video game":
+                        newGame();
+                        break;
+                    case "Exit \n":
+                        exit();
+                        break;
+                }
+            })
         }
 
-        function createMedia() {
-            if (!fs.existsSync(OUTPUT_DIR)) {
-                fs.mkdirSync(OUTPUT_DIR)
-            }
-            fs.writeFileSync(outputPath, render(save_VG), 'utf-8');
+        function orderBy_all() {
+            //left join to pull dept name from dept id
+            connection.query("SELECT * FROM products LEFT JOIN departments on products.department = departments.dept_id GROUP BY item_id", function (err, data) {
+                if (err) throw err;
+                //using easy-table to display the joined data
+                var t = new Table;
+                data.forEach(function (product) {
+                    t.cell('Item ID', product.item_id)
+                    t.cell('Product Name', product.product_name)
+                    t.cell('Department', product.dept_name)
+                    t.cell('Price, USD', product.price, Table.number(2))
+                    t.cell('Quantity', product.stock_quantity)
+                    t.newRow()
+                })
+                console.log(t.toString())
+                managerView();
+            })
         }
+        function orderBy_year() {
+            connection.query("SELECT * FROM products LEFT JOIN departments on products.department = departments.dept_id WHERE products.stock_quantity <5", function (err, data) {
+                if (err) throw err;
+                //using easy-table to display the joined data
+                var t = new Table;
+                data.forEach(function (product) {
+                    t.cell('Item ID', product.item_id)
+                    t.cell('Product Name', product.product_name)
+                    t.cell('Department', product.dept_name)
+                    t.cell('Price, USD', product.price, Table.number(2))
+                    t.cell('Quantity', product.stock_quantity)
+                    t.newRow()
+                })
+                console.log(t.toString())
+                managerView();
+            })
 
+        }
+        function updateGame() {
+            //left join to pull dept name from dept id
+            connection.query("SELECT * FROM products LEFT JOIN departments on products.department = departments.dept_id GROUP BY item_id", function (err, data) {
+                if (err) throw err;
+                //using easy-table to display the joined data
+                var t = new Table;
+                data.forEach(function (product) {
+                    t.cell('Item ID', product.item_id)
+                    t.cell('Product Name', product.product_name)
+                    t.cell('Department', product.dept_name)
+                    t.cell('Price, USD', product.price, Table.number(2))
+                    t.cell('Quantity', product.stock_quantity)
+                    t.newRow()
+                })
+                console.log("\n" + t.toString())
+                //inquirer prompts to set user actions
+                inquirer.prompt([
+                    {
+                        message: "Please enter the ID of the product you would like to restock",
+                        type: "input",
+                        name: "product"
+                    },
+                    {
+                        message: "How many would you like to add?",
+                        type: "input",
+                        name: "number"
+                    }
+
+                ]).then(function (answer) {
+                    //database is 1 indexed, and array 0 indexed, setting product to the JSON object pulled from our SQL db
+                    var product = data[(answer.product - 1)];
+                    var num = parseInt(answer.number);
+                    //sets stock quantity to current + amt added
+                    var newNum = parseInt((product.stock_quantity + num));
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: newNum
+                            },
+                            {
+                                item_id: product.item_id
+                            }
+                        ],
+                        //logs success/failure and the total for the transaction (num desired*price of prod)
+                        function (error) {
+                            if (error) throw error;
+                            var stock = product.stock_quantity;
+                            console.log("Restock successful!");
+                            orderBy_all();
+                            // managerView();
+                        }
+                    )
+
+                })
+            })
+        }
+        function newGame() {
+            //empty array to dynamically populate current departments
+            var deptChoices = [];
+            //selects all departments, pushes the id and name for each into the array
+            connection.query("SELECT * FROM departments", function (err, data) {
+                if (err) throw err;
+                for (i = 0; i < data.length; i++) {
+                    deptChoices.push(data[i].dept_id + " - " + data[i].dept_name)
+                }
+            })
+            inquirer.prompt([
+                {
+                    message: "What product would you like to add?",
+                    type: "input",
+                    name: "product"
+                },
+                {
+                    message: "What department does it belong in?",
+                    type: "list",
+                    //inquirer choices accept arrays, using the one created above
+                    choices: deptChoices,
+                    name: "dept"
+                },
+                {
+                    message: "Retail price of product?",
+                    type: "input",
+                    name: "price"
+                },
+                {
+                    message: "How many to add?",
+                    type: "input",
+                    name: "count"
+                }
+            ]).then(function (answer) {
+                var newProduct = answer.product;
+                console.log(newProduct);
+                //sets dept equal to the first index of the string chosen from our array 'deptChoices
+                //since the array is created as "dept_id + ..." this will always be our id
+                var dept = parseInt(answer.dept[0]);
+                var price = parseFloat(answer.price);
+                var quant = parseInt(answer.count);
+                connection.query(
+                    "INSERT INTO products (product_name, department, price, stock_quantity, product_sales) VALUES(?,?,?,?,0.00)",
+                    [
+                        newProduct,
+                        dept,
+                        price,
+                        quant
+                    ],
+                    function (err) {
+                        if (err) throw err;
+                        orderBy_all();
+                    })
+            })
+        }
+        function exit() {
+            connection.end();
+        }
         start_program();
     }
-    appMenu();
-
-    // GET: API URL (Read all video games) @ INSOMNIA
-    app.get('/api/video-games', (req,res) => {
-        const sql = `SELECT id, name AS title FROM game`;
-
-        db.query(sql, (err, rows) => {
-           if (err) {
-               res.status(500).json({error: err.message});
-               return;
-           }
-           res.json({
-               message: 'Success',
-               data: rows
-           });
-        });
-    });
-
-    // POST: API URL (New video game) @ INSOMNIA
-    app.post('/api/new',({body},res) => {
-        const sql = `INSERT INTO game (name) VALUES (?)`;
-        const params = [body.name];
-
-        db.query(sql,params,(err,result) => {
-            if (err) {
-                res.status(404).json({error: err.message});
-                return;
-            }
-            res.json({
-                message: 'Success',
-                data: body
-            });
-        });
-    });
-
-    // PUT: API URL (Update video game) @ INSOMNIA
-    app.put('/api/update/:id', (req,res) => {
-        const sql = `UPDATE game SET name = ? WHERE id = ?`;
-        const params = [req.body.name, req.params.id];
-
-        db.query(sql,params,(err,result) => {
-            if (err) {
-                res.status(404).json({error: err.message});
-            }
-            else if (!result.affectedRows){
-                res.json({
-                    message: 'Game Not Found'
-                });
-            }
-            else {
-                res.json({
-                    message: 'Success',
-                    data: req.body,
-                    changes: result.affectedRows
-                });
-            }
-        })
-    });
-
-    // DELETE: API URL @ INSOMNIA
-    app.delete('/api/delete/:id',(req,res) => {
-        const sql = `DELETE FROM game WHERE id = ?`;
-        const params = [req.params.id];
-
-        db.query(sql,params,(err,result) => {
-            if (err) {
-                res.readAsText(400).json({error: res.message});
-            }
-            else if (!result.affectedRows) {
-                res.json({
-                    message: 'Game Not Found'
-                });
-            }
-            else {
-                res.json({
-                    message: 'Deleted',
-                    changes: result.affectedRows,
-                    id: req.params.id
-                });
-            }
-        });
-    });
-
-    app.use((req, res) => {
-        res.status(404).end();
-    });
-
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-
-
-    // Express is a backend for Node JS to create web apps and APIs.
-    // MySQL is the most famous database service and the package allows Node JS connection.
